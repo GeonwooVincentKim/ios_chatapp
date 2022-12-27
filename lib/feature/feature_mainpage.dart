641c03987585af1b4f6eca0ad2a4e7a819a3f5ed
GeonwooVincentKim <kdsnop@gmail.com>
2022-12-27 16:45:06 +0900

    [Update] - Go to old version

--- lib/feature/feature_mainpage.dart
+++ lib/feature/feature_mainpage.dart
@@ -2,8 +2,6 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:ios_chatapp/feature/app_screens/first_tab_page.dart';
-import 'package:ios_chatapp/feature/app_screens/second_tab_page.dart';
 import 'package:ios_chatapp/feature/utils.dart';
 
 class MainPage extends StatefulWidget {
@@ -15,14 +13,12 @@ class MainPage extends StatefulWidget {
   State<MainPage> createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage> with SingleTickerProviderStateMixin {
+class _MainPageState extends State<MainPage> {
   late StreamSubscription subscription;
-  late TabController tabController;
 
   @override
   void initState() {
     super.initState();
-    tabController = TabController(length: 2, vsync: this);
     subscription = Connectivity().onConnectivityChanged.listen(showConnectivitySnackBar);
   }
 
@@ -39,39 +35,16 @@ class _MainPageState extends State<MainPage> with SingleTickerProviderStateMixin
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: TabBarView(
-        children: [
-          FirstTabPage(),
-          SecondTabPage()
-          // ElevatedButton(
-          //   style: ElevatedButton.styleFrom(padding: const EdgeInsets.all(12)),
-          //   child: const Text('Check Connection', style: TextStyle(fontSize: 20)),
-          //   onPressed: () async {
-          //     final result = await Connectivity().checkConnectivity();
-          //     showConnectivitySnackBar(result);
-          //   },
-          // ),
-        ],
-        controller: tabController,
-      ),
-      bottomNavigationBar: TabBar(
-        tabs: [
-          Tab(icon: Icon(Icons.looks_one, color: Colors.blue)),
-          Tab(icon: Icon(Icons.looks_two, color: Colors.blue))
-        ],
-      
-      ),
-
-      // body: Center(
-      //   child: ElevatedButton(
-      //     style: ElevatedButton.styleFrom(padding: const EdgeInsets.all(12)),
-      //     child: const Text('Check Connection', style: TextStyle(fontSize: 20)),
-      //     onPressed: () async {
-      //       final result = await Connectivity().checkConnectivity();
-      //       showConnectivitySnackBar(result);
-      //     },
-      //   )
-      // )
+      body: Center(
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(padding: const EdgeInsets.all(12)),
+          child: const Text('Check Connection', style: TextStyle(fontSize: 20)),
+          onPressed: () async {
+            final result = await Connectivity().checkConnectivity();
+            showConnectivitySnackBar(result);
+          },
+        )
+      )
     );
   }
 
