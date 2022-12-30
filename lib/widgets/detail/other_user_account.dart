230fa855e1a4e4842ecabbff87c7326341a7f4f3
GeonwooVincentKim <kdsnop@gmail.com>
2022-12-30 10:54:23 +0900

    [Feature] - Create new file, other_user_account

---
+++ lib/widgets/detail/other_user_account.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class OtherUserAccount extends StatelessWidget {
+  const OtherUserAccount({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: Container(
+        
+      )
+    );
+  }
+}
