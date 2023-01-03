349a1ba2bce97b3f9bf75a967d2f8d8bafb97ee8
GeonwooVincentKim <kdsnop@gmail.com>
2023-01-03 11:37:37 +0900

    [Feature] - Cupertino Chat ScrollView feature

--- lib/app_screens/cupertino_chat.dart
+++ lib/app_screens/cupertino_chat.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:ios_chatapp/model/users.dart';
 import 'package:ios_chatapp/provider/user_provider.dart';
 import 'package:ios_chatapp/widgets/cupertino_scroll_view/cupertino_sliver_grid.dart';
@@ -55,23 +56,65 @@ class _CupertinoChatState extends State<CupertinoChat> {
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        // CupertinoSliverNaviBar(textLocation: widget.textLocation),
-        SliverToBoxAdapter(
-            child: FractionallySizedBox(
-                widthFactor: 0.9,
-                child: ClipRect(
-                    child: Padding(
-                        padding: const EdgeInsets.only(top: 16),
-                        child: CupertinoSearchTextField(
-                          controller: _controller,
-                          onChanged: (value) => _updateUserList(value),
-                          onSubmitted: (value) => _updateUserList(value),
-                          onSuffixTap: () => _updateUserList(''),
-                        ))))),
-        CupertinoSliverGrid(userList: userList),
-      ],
+    return CupertinoPageScaffold(
+      // navigationBar: const CupertinoNavigationBar(
+      //   middle: Text('Friends', style: TextStyle(color: Colors.black)),
+      //   border: Border(bottom: BorderSide(color: Colors.transparent))
+      // ),
+      // appBar: const CupertinoNavigationBar(
+      //   middle: Text('Friends', style: TextStyle(color: Colors.black)),
+      //   border: Border(bottom: BorderSide(color: Colors.transparent)),
+      //   // trailing: CupertinoButton(
+      //   //   padding: EdgeInsets.zero,
+      //   //   child: const Text('confirm', style: TextStyle(color: CupertinoColors.white)),
+      //   //   onPressed: () {},
+      //   // ),
+      // ),
+      // navigationBar: const CupertinoNavigationBar(
+      //   middle: Text('Friends', style: TextStyle(color: CupertinoColors.opaqueSeparator)),
+      //   border: Border(bottom: BorderSide(color: CupertinoColors.quaternarySystemFill))
+      // ),
+      child: CustomScrollView(
+        slivers: [
+          const CupertinoSliverNavigationBar(
+            largeTitle: Text('Friends')
+          ),
+          // CupertinoSliverNavigationBar(
+          //   backgroundColor: CupertinoColors.darkBackgroundGray,
+          //   leading: CupertinoNavigationBarBackButton(
+          //     onPressed: () {},
+          //     color: CupertinoColors.activeBlue,
+          //   ),
+          //   middle: const Text('Friends', style: TextStyle(color: CupertinoColors.inactiveGray)),
+          //   trailing: CupertinoButton(
+          //     padding: EdgeInsets.zero,
+          //     child: const Text("Confirm", style: TextStyle(color: CupertinoColors.activeBlue)),
+          //     onPressed: () {},
+          //   ),
+          //   largeTitle: const Padding(
+          //     padding: EdgeInsets.only(left: 20),
+          //     child: Text("Large Tile", style: TextStyle(
+          //       color: CupertinoColors.white
+          //     ))
+          //   ),
+          //   previousPageTitle: "Back",
+          // ),
+          // CupertinoSliverNaviBar(textLocation: widget.textLocation),
+          SliverToBoxAdapter(
+              child: FractionallySizedBox(
+                  widthFactor: 0.9,
+                  child: ClipRect(
+                      child: Padding(
+                          padding: const EdgeInsets.only(top: 16),
+                          child: CupertinoSearchTextField(
+                            controller: _controller,
+                            onChanged: (value) => _updateUserList(value),
+                            onSubmitted: (value) => _updateUserList(value),
+                            onSuffixTap: () => _updateUserList(''),
+                          ))))),
+          CupertinoSliverGrid(userList: userList),
+        ],
+      )
     );
   }
 }
