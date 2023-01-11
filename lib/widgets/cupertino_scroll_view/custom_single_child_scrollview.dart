7a2b6b83e8c305f2c6e06489a2c2308d253d6d5c
GeonwooVincentKim <kdsnop@gmail.com>
2023-01-11 17:54:08 +0900

    [Update] - Finish to implement other user profile.dart page

--- lib/widgets/cupertino_scroll_view/custom_single_child_scrollview.dart
+++ lib/widgets/cupertino_scroll_view/custom_single_child_scrollview.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ios_chatapp/model/users.dart';
 import 'package:ios_chatapp/shared/style.dart';
@@ -21,12 +22,33 @@ class CustomSingleChildScrollView extends StatelessWidget {
         constraints: BoxConstraints(maxHeight: MediaQuery.of(context).size.height),
         color: Colors.amber,
         padding: const EdgeInsets.only(top: profileCircleAvatar),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          // mainAxisAlignment: MainAxisAlignment.start,
+        child: Stack(
+          alignment: Alignment.center,
           children: [
-            BoxColumnText(innerText: selectedUser!.name, checkTextIsTitle: true),
-            BoxColumnText(innerText: "Oh Happy Day~", checkTextIsTitle: false),
+            Column(
+              // mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                BoxColumnText(innerText: selectedUser!.name, checkTextIsTitle: true),
+                BoxColumnText(innerText: "Oh Happy Day~", checkTextIsTitle: false),
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.05, 
+                    // bottom: MediaQuery.of(context).size.height * 0.2
+                  )
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: const CircleBorder(),
+                    foregroundColor: Colors.black87,
+                    backgroundColor: Colors.white70
+                  ),
+                  child: const Icon(CupertinoIcons.pencil, color: black26),
+                  onPressed: () {
+                    // Navigator.pushNamed(context, "/")
+                  },
+                )
+              ],
+            )
           ],
         ),
       )
