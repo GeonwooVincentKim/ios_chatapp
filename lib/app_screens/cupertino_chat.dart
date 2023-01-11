54acbb92bd332acee89d01420a36aa97bacd12ea
GeonwooVincentKim <kdsnop@gmail.com>
2023-01-11 16:59:23 +0900

    [Update] - Delete unuse codes

--- lib/app_screens/cupertino_chat.dart
+++ lib/app_screens/cupertino_chat.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:ios_chatapp/model/users.dart';
 import 'package:ios_chatapp/provider/user_provider.dart';
-// import 'package:ios_chatapp/widgets/cupertino_scroll_view/cupertino_sliver_grid.dart';
 import 'package:ios_chatapp/widgets/custom/tile/user_tile.dart';
 
 class CupertinoChat extends StatefulWidget {
@@ -55,68 +54,13 @@ class _CupertinoChatState extends State<CupertinoChat> {
 
   @override
   Widget build(BuildContext context) {
-    print('Click');
     return CupertinoPageScaffold(
-      // navigationBar: const CupertinoNavigationBar(
-      //   middle: Text('Friends', style: TextStyle(color: Colors.black)),
-      //   border: Border(bottom: BorderSide(color: Colors.transparent))
-      // ),
-      // appBar: const CupertinoNavigationBar(
-      //   middle: Text('Friends', style: TextStyle(color: Colors.black)),
-      //   border: Border(bottom: BorderSide(color: Colors.transparent)),
-      //   // trailing: CupertinoButton(
-      //   //   padding: EdgeInsets.zero,
-      //   //   child: const Text('confirm', style: TextStyle(color: CupertinoColors.white)),
-      //   //   onPressed: () {},
-      //   // ),
-      // ),
-      // navigationBar: const CupertinoNavigationBar(
-      //   middle: Text('Friends', style: TextStyle(color: CupertinoColors.opaqueSeparator)),
-      //   border: Border(bottom: BorderSide(color: CupertinoColors.quaternarySystemFill))
-      // ),
       child: CustomScrollView(
         slivers: [
           CupertinoSliverNavigationBar(
             // largeTitle: Text('Friends')
             largeTitle: Text(widget.textLocation)
           ),
-          // CupertinoSliverNavigationBar(
-          //   backgroundColor: CupertinoColors.darkBackgroundGray,
-          //   leading: CupertinoNavigationBarBackButton(
-          //     onPressed: () {},
-          //     color: CupertinoColors.activeBlue,
-          //   ),
-          //   middle: const Text('Friends', style: TextStyle(color: CupertinoColors.inactiveGray)),
-          //   trailing: CupertinoButton(
-          //     padding: EdgeInsets.zero,
-          //     child: const Text("Confirm", style: TextStyle(color: CupertinoColors.activeBlue)),
-          //     onPressed: () {},
-          //   ),
-          //   largeTitle: const Padding(
-          //     padding: EdgeInsets.only(left: 20),
-          //     child: Text("Large Tile", style: TextStyle(
-          //       color: CupertinoColors.white
-          //     ))
-          //   ),
-          //   previousPageTitle: "Back",
-          // ),
-          // CupertinoSliverNaviBar(textLocation: widget.textLocation),
-          // SliverToBoxAdapter(
-          //   child: FractionallySizedBox(
-          //     widthFactor: 0.9,
-          //     child: ClipRect(
-          //       child: Padding(
-          //         padding: const EdgeInsets.only(top: 16),
-          //         child: CupertinoSearchTextField(
-          //           controller: _controller,
-          //           onChanged: (value) => _updateUserList(value),
-          //           onSubmitted: (value) => _updateUserList(value),
-          //           onSuffixTap: () => _updateUserList(''),
-          //         )
-          //       )
-          //     )
-          //   )
-          // ),
           SliverGrid(
             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 1,
