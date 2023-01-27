1ad3e09c0c64555747a83960f38f5f40675304e7
GeonwooVincentKim <kdsnop@gmail.com>
2023-01-27 13:20:32 +0900

    [Update] - Updated river_pod example

---
+++ lib/feature_riverpod/new_todo_list/filters/riverpod_filters.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ios_chatapp/feature_riverpod/new_todo_list/todo.dart';
+
+/// Some keys used for testing
+final addTodoKey = UniqueKey();
+final activeFilterKey = UniqueKey();
+final completedFilterKey = UniqueKey();
+final allFilterKey = UniqueKey();
+
+/// Creates a [TodoList] and initialise it with pre-defined values.
+///
+/// We are using [StateNotifierProvider] here as a `List<Todo>` is a complex
+/// object, with advanced business logic like how to edit a todo.
+final todoListProvider = StateNotifierProvider<TodoList, List<Todo>>((ref) {
+  return TodoList(const [
+    Todo(id: 'todo-0', description: 'hi'),
+    Todo(id: 'todo-1', description: 'hello'),
+    Todo(id: 'todo-2', description: 'bonjour'),
+  ]);
+});
+
+/// The different ways to filter the list of todos
+enum TodoListFilter {
+  all,
+  active,
+  completed,
+}
+
+/// The currently active filter.
+///
+/// We use [StateProvider] here as there is no fancy logic behind manipulating
+/// the value since it's just enum.
+final todoListFilter = StateProvider((_) => TodoListFilter.all);
+
+/// The number of uncompleted todos
+///
+/// By using [Provider], this value is cached, making it performant.\
+/// Even multiple widgets try to read the number of uncompleted todos,
+/// the value will be computed only once (until the todo-list changes).
+///
+/// This will also optimise unneeded rebuilds if the todo-list changes, but the
+/// number of uncompleted todos doesn't (such as when editing a todo).
+final uncompletedTodosCount = Provider<int>((ref) {
+  return ref.watch(todoListProvider).where((todo) => !todo.completed).length;
+});
+
+/// The list of todos after applying of [todoListFilter].
+///
+/// This too uses [Provider], to avoid recomputing the filtered list unless either
+/// the filter of or the todo-list updates.
+final filteredTodos = Provider<List<Todo>>((ref) {
+  final filter = ref.watch(todoListFilter);
+  final todos = ref.watch(todoListProvider);
+
+  switch (filter) {
+    case TodoListFilter.completed:
+      return todos.where((todo) => todo.completed).toList();
+    case TodoListFilter.active:
+      return todos.where((todo) => !todo.completed).toList();
+    case TodoListFilter.all:
+      return todos;
+  }
+});
