d425454c53b1ceb8d143afcad47a5f156c685701
GeonwooVincentKim <kdsnop@gmail.com>
2023-01-27 14:27:00 +0900

    [Update] - Separated custom_tool_tip.dart

--- lib/feature_riverpod/new_todo_list/widgets/tool_bar.dart
+++ lib/feature_riverpod/new_todo_list/widgets/tool_bar.dart
@@ -2,11 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ios_chatapp/feature_riverpod/new_todo_list/filters/riverpod_filters.dart';
 import 'package:ios_chatapp/feature_riverpod/new_todo_list/shared/util.dart';
+import 'package:ios_chatapp/feature_riverpod/new_todo_list/widgets/custom_tool_tip.dart';
 
 class Toolbar extends HookConsumerWidget {
-  const Toolbar({
-    Key? key,
-  }) : super(key: key);
+  const Toolbar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -48,40 +47,6 @@ class Toolbar extends HookConsumerWidget {
             textContents: "Completed"
           ),
         ],
-      ),
-    );
-  }
-}
-
-class CustomToolTip extends StatelessWidget {
-  final UniqueKey getKey;
-  final String message;
-  final WidgetRef ref;
-  final Color? textColor;
-  final String textContents;
-
-  const CustomToolTip({
-    super.key,
-    required this.getKey,
-    required this.message,
-    required this.ref,
-    required this.textColor,
-    required this.textContents,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Tooltip(
-      key: getKey,
-      message: 'Only uncompleted todos',
-      child: TextButton(
-        onPressed: () => ref.read(todoListFilter.notifier).state =
-            filterButtonState(textContents),
-        style: ButtonStyle(
-          visualDensity: VisualDensity.compact,
-          foregroundColor: MaterialStateProperty.all(textColor),
-        ),
-        child: Text(textContents),
       ),
     );
   }
