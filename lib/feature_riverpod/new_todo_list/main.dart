aafdeb7283e78832a2c082fe6b9aacf621d4c14f
GeonwooVincentKim <kdsnop@gmail.com>
2023-01-27 14:28:50 +0900

    [Update] - useIsFocused set to the util.dart file

--- lib/feature_riverpod/new_todo_list/main.dart
+++ lib/feature_riverpod/new_todo_list/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ios_chatapp/feature_riverpod/new_todo_list/filters/riverpod_filters.dart';
+import 'package:ios_chatapp/feature_riverpod/new_todo_list/shared/util.dart';
 import 'package:ios_chatapp/feature_riverpod/new_todo_list/widgets/title.dart';
 import 'package:ios_chatapp/feature_riverpod/new_todo_list/widgets/tool_bar.dart';
 
@@ -131,22 +132,4 @@ class TodoItem extends HookConsumerWidget {
       ),
     );
   }
-}
-
-bool useIsFocused(FocusNode node) {
-  final isFocused = useState(node.hasFocus);
-
-  useEffect(
-    () {
-      void listener() {
-        isFocused.value = node.hasFocus;
-      }
-
-      node.addListener(listener);
-      return () => node.removeListener(listener);
-    },
-    [node],
-  );
-
-  return isFocused.value;
 }
--- lib/feature_riverpod/new_todo_list/shared/util.dart
+++ lib/feature_riverpod/new_todo_list/shared/util.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:ios_chatapp/feature_riverpod/new_todo_list/filters/riverpod_filters.dart';
 
 TodoListFilter filterButtonState(textContents) {
@@ -10,4 +12,22 @@ TodoListFilter filterButtonState(textContents) {
   } else {
     return TodoListFilter.active; 
   }
+}
+
+bool useIsFocused(FocusNode node) {
+  final isFocused = useState(node.hasFocus);
+
+  useEffect(
+    () {
+      void listener() {
+        isFocused.value = node.hasFocus;
+      }
+
+      node.addListener(listener);
+      return () => node.removeListener(listener);
+    },
+    [node],
+  );
+
+  return isFocused.value;
 }
