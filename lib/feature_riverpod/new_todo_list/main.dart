9ec1caa158f69293de8de8d124d96ca30822c311
GeonwooVincentKim <kdsnop@gmail.com>
2023-01-27 14:32:21 +0900

    [Update] - Main.dart and home.dart separation

--- lib/feature_riverpod/new_todo_list/main.dart
+++ lib/feature_riverpod/new_todo_list/main.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:ios_chatapp/feature_riverpod/new_todo_list/filters/riverpod_filters.dart';
-import 'package:ios_chatapp/feature_riverpod/new_todo_list/shared/util.dart';
-import 'package:ios_chatapp/feature_riverpod/new_todo_list/widgets/title.dart';
-import 'package:ios_chatapp/feature_riverpod/new_todo_list/widgets/tool_bar.dart';
-
-import 'todo.dart';
+import 'package:ios_chatapp/feature_riverpod/new_todo_list/screens/home.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -19,117 +13,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Home(),
-    );
-  }
-}
-
-class Home extends HookConsumerWidget {
-  const Home({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final todos = ref.watch(filteredTodos);
-    final newTodoController = useTextEditingController();
-
-    return GestureDetector(
-      onTap: () => FocusScope.of(context).unfocus(),
-      child: Scaffold(
-        body: ListView(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-          children: [
-            const RiverPodTitle(),
-            TextField(
-              key: addTodoKey,
-              controller: newTodoController,
-              decoration: const InputDecoration(
-                labelText: 'What needs to be done?',
-              ),
-              onSubmitted: (value) {
-                ref.read(todoListProvider.notifier).add(value);
-                newTodoController.clear();
-              },
-            ),
-            const SizedBox(height: 42),
-            const Toolbar(),
-            if (todos.isNotEmpty) const Divider(height: 0),
-            for (var i = 0; i < todos.length; i++) ...[
-              if (i > 0) const Divider(height: 0),
-              Dismissible(
-                key: ValueKey(todos[i].id),
-                onDismissed: (_) {
-                  ref.read(todoListProvider.notifier).remove(todos[i]);
-                },
-                child: ProviderScope(
-                  overrides: [
-                    _currentTodo.overrideWithValue(todos[i]),
-                  ],
-                  child: const TodoItem(),
-                ),
-              )
-            ],
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-
-/// A provider which exposes the [Todo] displayed by a [TodoItem].
-///
-/// By retrieving the [Todo] through a provider instead of through its
-/// constructor, this allows [TodoItem] to be instantiated using the `const` keyword.
-///
-/// This ensures that when we add/remove/edit todos, only what the
-/// impacted widgets rebuilds, instead of the entire list of items.
-final _currentTodo = Provider<Todo>((ref) => throw UnimplementedError());
-
-class TodoItem extends HookConsumerWidget {
-  const TodoItem({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final todo = ref.watch(_currentTodo);
-    final itemFocusNode = useFocusNode();
-    final itemIsFocused = useIsFocused(itemFocusNode);
-
-    final textEditingController = useTextEditingController();
-    final textFieldFocusNode = useFocusNode();
-
-    return Material(
-      color: Colors.white,
-      elevation: 6,
-      child: Focus(
-        focusNode: itemFocusNode,
-        onFocusChange: (focused) {
-          if (focused) {
-            textEditingController.text = todo.description;
-          } else {
-            // Commit changes only when the textfield is unfocused, for performance
-            ref
-                .read(todoListProvider.notifier)
-                .edit(id: todo.id, description: textEditingController.text);
-          }
-        },
-        child: ListTile(
-          onTap: () {
-            itemFocusNode.requestFocus();
-            textFieldFocusNode.requestFocus();
-          },
-          leading: Checkbox(
-            value: todo.completed,
-            onChanged: (value) =>
-                ref.read(todoListProvider.notifier).toggle(todo.id),
-          ),
-          title: itemIsFocused
-              ? TextField(
-                  autofocus: true,
-                  focusNode: textFieldFocusNode,
-                  controller: textEditingController,
-                )
-              : Text(todo.description),
-        ),
-      ),
     );
   }
 }
