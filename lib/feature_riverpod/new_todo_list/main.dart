6729b67441a0b61aa14b677024710a13bf82b32d
GeonwooVincentKim <kdsnop@gmail.com>
2023-01-27 13:25:41 +0900

    [Update] - Title.dart has just updated

--- lib/feature_riverpod/new_todo_list/main.dart
+++ lib/feature_riverpod/new_todo_list/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ios_chatapp/feature_riverpod/new_todo_list/filters/riverpod_filters.dart';
+import 'package:ios_chatapp/feature_riverpod/new_todo_list/widgets/title.dart';
 
 import 'todo.dart';
 
@@ -34,7 +35,7 @@ class Home extends HookConsumerWidget {
         body: ListView(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
           children: [
-            const Title(),
+            const RiverPodTitle(),
             TextField(
               key: addTodoKey,
               controller: newTodoController,
@@ -139,24 +140,6 @@ class Toolbar extends HookConsumerWidget {
             ),
           ),
         ],
-      ),
-    );
-  }
-}
-
-class Title extends StatelessWidget {
-  const Title({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Text(
-      'todos',
-      textAlign: TextAlign.center,
-      style: TextStyle(
-        color: Color.fromARGB(38, 47, 47, 247),
-        fontSize: 100,
-        fontWeight: FontWeight.w100,
-        fontFamily: 'Helvetica Neue',
       ),
     );
   }
