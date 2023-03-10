2591af7fc08931040fc52adb7f1976ef2040a49e
GeonwooVincentKim <kdsnop@gmail.com>
2023-03-10 14:06:12 +0900

    [Update] - Delete unuse codes

--- lib/app_screens/call/add_call_page.dart
+++ lib/app_screens/call/add_call_page.dart
@@ -12,13 +12,22 @@ class AddCallPage extends StatefulWidget {
 class _AddCallPageState extends State<AddCallPage> {
   late User callUser;
 
-  final _formKey = GlobalKey<FormState>();
+  Color _randomColor = CupertinoColors.black;
 
+  final nameController = TextEditingController();
+  final colorController = TextEditingController();
+  final phoneController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+  Map<String, dynamic> newUser = {
+    'name': '',
+    'phoneNumber': '',
+  };
+  
   @override
   void initState() {
     super.initState();
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -35,22 +44,6 @@ class _AddCallPageState extends State<AddCallPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // Padding(
-              //   padding: EdgeInsets.all(value),
-              //   child: Row(
-              //     children: const [
-              //       Expanded(child: Text("Test")),
-              //       Expanded(child: Text("Test"))
-              //       // TextFormField()
-              //     ],
-              //   ),
-              // )
-              // CupertinoTextFormFieldRow(
-              //   controller: controller,
-              //   decoration: InputDecoration(
-
-              //   )
-              // )
               CupertinoFormSection.insetGrouped(
                 header: Text('Enter your name:'),
                 children: [
@@ -77,42 +70,6 @@ class _AddCallPageState extends State<AddCallPage> {
                   ),
                 ],
               ),
-              // Row(
-              //   mainAxisAlignment: MainAxisAlignment.spaceAround,
-              //   children: [
-              //     Container(
-              //       child: CupertinoTextFormFieldRow(
-              //         prefix: Icon(
-              //           CupertinoIcons.person,
-              //           color: CupertinoColors.systemGrey,
-              //         ),
-              //         placeholder: 'Name',
-              //         controller: _controller
-              //       ),
-              //     ),
-              //   ],
-              // )
-              // Row(
-              //   children: [
-              //     Container(
-              //       width: MediaQuery.of(context).size.width * 0.5,
-              //       // child: CupertinoFormSection.insetGrouped(
-              //       //   header: Text("Enter your name"),
-              //       //   children: [
-              //           child: CupertinoTextFormFieldRow(
-              //             prefix: Icon(
-              //               CupertinoIcons.person,
-              //               color: CupertinoColors.systemGrey,
-              //             ),
-              //             placeholder: 'Name',
-              //             controller: _controller
-              //           )
-              //       //   ],
-              //       // )
-              //     )
-              //   ]
-              // )
-              // Text(widget.callInfo.)
             ],
           ),
         ),
